553fdcd01b4f7d12da818a7421dfa1319b2bc56c
civic1007 <civic1007@proton.me>
2024-07-15 18:28:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,20 @@ function displayImageButtons() {
 }
 
 document.addEventListener('DOMContentLoaded', displayImageButtons);
+document.onkeydown = (e) => {
+    if (e.key == 123) {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.key == 'U') {
+        e.preventDefault();
+    }
+};
